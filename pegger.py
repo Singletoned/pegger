74b3f15f7da1b2c72fe5de41a71e5b4852565d86
Ed Singleton <singletoned@gmail.com>
2011-01-10 23:36:14 +0000

    Added a test for no match

--- pegger.py
+++ pegger.py
@@ -3,3 +3,5 @@
 def parse_string(text, pattern):
     if text == pattern():
         return [pattern.__name__, text]
+    else:
+        return None
--- test_pegger.py
+++ test_pegger.py
@@ -9,9 +9,15 @@ def test_parse_string_a():
     result = parse_string("a", letter_a)
     assert result == ['letter_a', "a"]
 
+    result = parse_string("c", letter_a)
+    assert result == None
+
 def test_parse_string_b():
     def letter_b():
         return "b"
 
     result = parse_string("b", letter_b)
     assert result == ['letter_b', "b"]
+
+    result = parse_string("c", letter_b)
+    assert result == None
