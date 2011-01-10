2c6b7afd57a2bc2dd5487540230a20d88058f0c3
Ed Singleton <singletoned@gmail.com>
2011-01-10 20:53:08 +0000

    Added trivial second test

--- pegger.py
+++ pegger.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 
 def parse_string(text, pattern):
-    return ['letter_a', "a"]
+    if text == pattern():
+        return [pattern.__name__, text]
--- test_pegger.py
+++ test_pegger.py
@@ -8,3 +8,10 @@ def test_parse_string_a():
 
     result = parse_string("a", letter_a)
     assert result == ['letter_a', "a"]
+
+def test_parse_string_b():
+    def letter_b():
+        return "b"
+
+    result = parse_string("b", letter_b)
+    assert result == ['letter_b', "b"]
