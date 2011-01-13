bbf22b04ac114a0e62ce8eb9fdb8f3f39d7b77c2
Ed Singleton <singletoned@gmail.com>
2011-01-13 10:12:23 +0000

    Broke code into some matching functions

--- pegger.py
+++ pegger.py
@@ -4,12 +4,48 @@ class Some(object):
     def __init__(self, pattern):
         self.pattern = pattern
 
-def parse_string(text, pattern):
-    if text == pattern():
-        return [pattern.__name__, text]
-    elif isinstance(pattern(), tuple):
-        return [parse_string(sub_text, sub_pattern) for sub_text, sub_pattern in zip(text, pattern())]
-    elif isinstance(pattern(), Some):
-        return [pattern.__name__, text]
+def match_some(text, pattern):
+    match = []
+    rest = list(text)
+    while rest:
+        char = rest[0]
+        if pattern().pattern == char:
+            match.append(rest.pop(0))
+        else:
+            break
+    return ([pattern.__name__, "".join(match)], "".join(rest))
+
+def match_text(text, pattern):
+    """If the pattern matches the beginning of the text, parser it and
+    return the rest"""
+    if text.startswith(pattern()):
+        rest = text[len(pattern()):]
+        return ([pattern.__name__, pattern()], rest)
     else:
-        return None
+        return (None, None)
+
+def match_tuple(text, pattern):
+    "Match each of the patterns in the tuple in turn"
+    result = []
+    rest = text
+    for sub_pattern in pattern():
+        match, rest = do_parse(rest, sub_pattern)
+        result.append(match)
+    return (result, rest)
+    
+
+def do_parse(text, pattern):
+    if text == pattern():
+        return ([pattern.__name__, text], '')
+    elif isinstance(pattern(), (str, unicode)):
+        return match_text(text, pattern)
+    elif isinstance(pattern(), tuple):
+        return match_tuple(text, pattern)
+    elif isinstance(pattern(), Some):
+        return match_some(text, pattern)
+    else:
+        return (None, None)
+
+def parse_string(text, pattern):
+    match, rest = do_parse(text, pattern)
+    return match
--- test_pegger.py
+++ test_pegger.py
@@ -2,11 +2,52 @@
 
 import pegger as pg
 
+def test_match_some():
+    def word_a():
+        return pg.Some('a')
+
+    match, rest = pg.match_some("a", word_a)
+    assert match == ['word_a', "a"]
+    assert rest == ""
+
+    match, rest = pg.match_some("aab", word_a)
+    assert match == ['word_a', "aa"]
+    assert rest == "b"
+
+def test_match_text():
+    """Test that a text pattern removes the pattern from the beginning
+    of the text and returns the rest"""
+
+    def letter_a():
+        return "a"
+    
+    result = pg.match_text("ab", letter_a)
+    assert result == (['letter_a', "a"], "b")
+
+    result = pg.match_text("aabc", letter_a)
+    assert result == (['letter_a', "a"], "abc")
+
+def test_match_tuple():
+    def letter_a():
+        return "a"
+
+    def letter_b():
+        return "b"
+
+    def word_ab():
+        return (letter_a, letter_b)
+
+    result = pg.match_tuple("ab", word_ab)
+    assert result == ([['letter_a', "a"], ['letter_b', "b"]], "")
+
 def test_parse_string_a():
     def letter_a():
         return "a"
 
     result = pg.parse_string("a", letter_a)
+    assert result == ['letter_a', "a"]
+
+    result = pg.parse_string("ab", letter_a)
     assert result == ['letter_a', "a"]
 
     result = pg.parse_string("c", letter_a)
@@ -44,3 +85,16 @@ def test_parse_string_some_a():
 
     result = pg.parse_string("aaa", word_a)
     assert result == ['word_a', "aaa"]
+
+def test_parse_string_some_aab():
+    def word_a():
+        return pg.Some('a')
+
+    def letter_b():
+        return "b"
+
+    def word_aab():
+        return (word_a, letter_b)
+
+    result = pg.parse_string("aab", word_aab)
+    assert result == [['word_a', "aa"], ['letter_b', "b"]]
